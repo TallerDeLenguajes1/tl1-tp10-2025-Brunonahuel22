74978ecc03246f842bd28a034c9b02c07833f781
Bruno Cordoba <brunonahuel87@gmail.com>
2025-06-27 17:23:21 -0300

    termine punto 1

---
+++ Tareas/ClaseTarea.cs
@@ -0,0 +1,11 @@
+namespace ClaseTarea
+{
+    class Tarea
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+        
+    }
+}
--- Tareas/Program.cs
+++ Tareas/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClaseTarea;
+using System.Text.Json;
+using System.IO;
+using System.Globalization;
+
+string url = "https://jsonplaceholder.typicode.com/todos/";
+
+HttpClient client = new HttpClient();
+
+HttpResponseMessage respuesta = await client.GetAsync(url);
+
+respuesta.EnsureSuccessStatusCode();
+
+string resonseBody = await respuesta.Content.ReadAsStringAsync();
+
+List<Tarea> listaTareas = JsonSerializer.Deserialize<List<Tarea>>(resonseBody);
+
+
+
+Console.WriteLine("\t TAREAS PENDIENTES");
+foreach (var tarea in listaTareas)
+{
+    if (tarea.completed == false)
+    {
+        Console.WriteLine($"Titulo: {tarea.title} Estado: {tarea.completed}");
+    }
+
+}
+
+
+Console.WriteLine("\t TAREAS COMPLETADAS");
+foreach (var tarea in listaTareas)
+{
+    if (tarea.completed == true)
+    {
+        Console.WriteLine($"Titulo: {tarea.title} Estado: {tarea.completed}");
+    }
+
+}
+
+string json = JsonSerializer.Serialize(listaTareas);
+
+string directorio = @"c:\Users\PC\Desktop\tl1-tp10-2025-Brunonahuel22\tareas.json";
+
+File.WriteAllText(directorio, json);
+if (File.Exists(directorio))
+{
+    Console.WriteLine("Archivo Creado");
+}
