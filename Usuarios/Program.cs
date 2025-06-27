5d3411847f365dddcef534eb57b52f3414492339
Bruno Cordoba <brunonahuel87@gmail.com>
2025-06-27 17:38:39 -0300

    ejercicio 2

---
+++ Usuarios/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+
+
+string url = "https://jsonplaceholder.typicode.com/users";
+HttpClient client = new HttpClient();
+
+HttpResponseMessage respuesta = await client.GetAsync(url);
+
+string cuerpoBody = await respuesta.Content.ReadAsStringAsync();
+
+Console.WriteLine(cuerpoBody);
+
